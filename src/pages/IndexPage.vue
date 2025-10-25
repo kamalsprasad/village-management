dd43180177152f05dbd7d487b80a4c5289d01312
Kamal Prasad <ecofriend@yahoo.com>
2025-10-25 19:24:28 +1100

    prettier config

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <div class="text-center">
-      <h1 class="text-h3 text-primary q-mb-md">
-        Sustainable Model Village Management System
-      </h1>
+      <h1 class="text-h3 text-primary q-mb-md">Sustainable Model Village Management System</h1>
       <p class="text-h6 text-grey-7 q-mb-lg">
         Transforming rural African villages through data-driven management
       </p>
@@ -19,15 +17,15 @@
 </template>
 
 <script setup>
-import { useQuasar } from 'quasar'
+import { useQuasar } from 'quasar';
 
-const $q = useQuasar()
+const $q = useQuasar();
 
 function handleGetStarted() {
   $q.notify({
     message: 'Welcome to Village Management System!',
     color: 'positive',
     position: 'top',
-  })
+  });
 }
 </script>
